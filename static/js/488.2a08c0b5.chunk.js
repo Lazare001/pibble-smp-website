b2f978b7aabb07a5d10cd0a8bf70065f46a44e43
Lazare001 <96613022+Lazare001@users.noreply.github.com>
2025-10-25 21:56:08 +0400

    Updates

--- static/js/488.2a08c0b5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpibble_smp_app=self.webpackChunkpibble_smp_app||[]).push([[488],{488:(p,e,s)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.2a08c0b5.chunk.js.map
